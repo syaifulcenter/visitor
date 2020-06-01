fb184ba82f93f5ed5b1354152c96ae562515192d
km37id <65444913+km37id@users.noreply.github.com>
2020-06-01 17:00:35 +0700

    Update antix.php

--- antix.php
+++ antix.php
@@ -94,100 +94,41 @@ function visitor($green2, $putih, $red, $suc, $vis, $data){
                           }
         }
 }
-}
-  echo "{$green2} [ 1 ] Single Domain\n";
-  echo " [ 2 ] Mass Domain List{$putih}\n";
-  $pass = readline(" [x] SELECT BOT [ 1/2 ]: ");sleep(1);
-
-  if ($pass == 1) {
-    $domain = readline("  ╚═ [-] Your Single Site: ");
-
-           function parsing($respon){
-                        $one = explode('<span class="red_bold">', $respon);
-                        $two = explode('</span>', $one[2]);
-  return $two[0];
-}
-          function mengambil($url, $header){
-                        $ch = curl_init();
-                              curl_setopt($ch, CURLOPT_URL, $url);
-                              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                              curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-                              curl_setopt($ch,CURLOPT_TIMEOUT,10);
-              $result = curl_exec($ch);
-                        curl_close($ch);
-                        sleep(3);
-  return $result;
-}
-            echo "\n{$red2}        Loading Check Your Visitor Site{$putih}";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo " Done\n\n{$putih}";
-
-                          $url = "https://www.statshow.com/www/.'$domain'";
-                          $res = mengambil($url, array());
-                          $suc = (parsing($res));
-
-        echo "{$green2}         Domain {$putih}$domain \n         {$green2}Visitor {$putih}$suc\n\n";die;
-  }else{
+} 
+  echo "{$green2} [ 1 ] Explode Site\n";
+  echo " [ 2 ] Single Check Visitor\n";
+  echo " [ 3 ] Mass Check List Visitor{$putih}\n";
+  $pass = readline(" [x] SELECT BOT [ 1/2/3 ]: ");sleep(1);
 
 
-    $keydat1 = readline("  ╚═ [x] Continue OR Don't Explode File:[ Y/N ] ");sleep(1);
-    $keydat2 = strtolower($keydat1);  
-    if ($keydat2 == "n") {
-                    $string = readline("      ╚═ [x] Your File: ");sleep(1);
-                    $vis = readline("         [x] Target Visitor: ");
-                    echo "\n{$red2}         Loading Read Your List Site{$putih}";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo " Done\n\n{$putih}";
-                    sleep(2);
-                    $data = $string;
-    visitor($green2, $putih, $red, $suc, $vis, $data);die;
+if ($pass == 1) {
 
-    }else{
+//input
+  $string = readline("    [-] File Explode: ");
+  $wfile = readline("    [-] Save: ");
+  mkdir("Result");
+  mkdir("Result/Explode");
+  $baris = fopen($string, "r");
+  $datex = "Result/Explode/EPD-".date("d-m-Y")."_@Antix";
+         $membuka = fopen($datex.".txt", 'w+');
+  echo "\n{$red2}        Loading Explode Your List{$putih}";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo " Done\n\n{$putih}";
+  while (true) {
+  $line = fgets($baris);
+  if($line == null )break;
 
-
-
-                    $string = readline("      ╚═ [+] Your Explode File: ");
-                    $baris = fopen($string, "r");
-                    $membuka = fopen('_tmp_.txt', 'w');
-                echo "\n{$red2}         Loading Explode Your List Site{$putih}";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo " Done\n\n";
-                sleep(2);
-
-      while (true) {
-               $line = fgets($baris);
-        if($line == null )break;
-                $str_arr =  explode ("/", $line);
-                $line = "$str_arr[2]";
-
-  // echo "{$green2}".$line."{$putih}\n";
+  $str_arr =  explode ("/", $line);
+  $line = "$str_arr[2]";
+  echo "\n{$green2} ".$line;
   fwrite($membuka, $line."\n");
 
+
+  // echo $line;
 }
+echo "{$putih}.";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo "Done\n";
 
-  fclose($line);
+  fclose($line);die;
 
-              $to2 = readline(" [x] continue to check site visitors:[ Y/N ]: ");sleep(1);
-              $key2 = strtolower($to2);  
-
-    if ($key2 == "n") {
-            echo "\n{$red2}        https://github.com/km37id/{$putih}";sleep(1);echo ".";sleep(1);echo "Close Program";sleep(1);die;
-    }
-    $vis = readline("  ╚═ [x] Target Visitor: ");
-    $data = "_tmp_.txt";
-    visitor($green2, $putih, $red, $suc, $vis, $data);die;
-
-    }
-
-  }
-
- ?>
-lse{
-        echo "{$green2}         Domain {$putih}$domain \n         {$green2}Visitor {$putih}$suc\n";
-                          fwrite($up, $domain."\n");
-                          }
-        }
-}
-}
-  echo "{$green2} [ 1 ] Single Domain\n";
-  echo " [ 2 ] Mass Domain List{$putih}\n";
-  $pass = readline(" [x] SELECT BOT [ 1/2 ]: ");sleep(1);
-
-  if ($pass == 1) {
+}elseif ($pass == 2) {
     $domain = readline("  ╚═ [-] Your Single Site: ");
 
            function parsing($respon){
